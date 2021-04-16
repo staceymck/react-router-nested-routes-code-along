a8f8cf8db17217295b9fa602d91e51b021ea87d9
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-16 15:24:37 -0500

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,10 +2,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
+import MovieShow from '../components/MovieShow'
 
 const MoviesPage = ({ match, movies }) => (
   <div>
+    <Route exact path={match.url} render={() => <h3>Choose a movie from the list</h3>} />
     <MoviesList movies={movies} />
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies}/>} />
   </div>
 )
 
